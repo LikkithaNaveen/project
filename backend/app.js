a4bdae22095ff9c7046e59c33d874b347ef323e8
Likkitha <likkithalikis01@gmail.com>
2024-04-03 16:40:39 +0530

    project

--- backend/app.js
+++ backend/app.js
@@ -14,16 +14,9 @@ app.use(cors())
 app.use("/api/user", userRouter);
 app.use(errorHandler)
 
-// app.post("/sign-in",
-//   (req, res, next) => {
-//     const { email, password } = req.body
-//     if (!email || !password)
-//       return res.json({ error: 'email/ password missing!' })
-//     next()
-//   },
-//   (req, res) => {
-//     res.send("<h1>Hello I am from your backend about</h1>");
-//   });
+app.get("/", (req, res) => {
+  res.status(200).json("Sucessfully connected");
+});
 
 app.listen(8000, () => {
   console.log("the port is listening on port 8000");
